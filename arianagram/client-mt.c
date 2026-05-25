f9c9b5b0cf5ed0680a5959cd757261ba0110f983
gdconstantino2 <125415504+gdconstantino2@users.noreply.github.com>
2026-05-25 10:04:02 -0300

    Refactor client-mt.c to use select for input handling

--- arianagram/client-mt.c
+++ arianagram/client-mt.c
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/select.h>
 
 #define BUFSZ 1024
 
@@ -46,7 +47,7 @@ int main(int argc, char **argv)
     Message msg;
     memset(&msg, 0, sizeof(msg));
     msg.type = MSG_CONNECT;
-    strncpy(msg.username, argv[3], USER_SIZE); // username da linha de comando
+    strncpy(msg.username, argv[3], USER_SIZE);
 
     if (send(s, &msg, sizeof(msg), 0) != sizeof(msg))
     {
@@ -65,79 +66,99 @@ int main(int argc, char **argv)
     char input[BUFSZ];
     char command[BUFSZ];
     char argument[BUFSZ];
-    int flags = fcntl(s, F_GETFL, 0);
-    fcntl(s, F_SETFL, flags | O_NONBLOCK);
     
-   while (1)
+    fd_set readfds;
+    int max_fd;
+
+    while (1)
     {
-        // Verifica notificações pendentes (sem bloquear)
-        Message push_msg;
-        bytes = recv(s, &push_msg, sizeof(push_msg), MSG_DONTWAIT);
-        while (bytes > 0) {
-            if (push_msg.type == MSG_PUSH) {
-                printf("[NOTIFICATION] %s: \"%s\"\n", 
-                       push_msg.username, push_msg.content);
-                fflush(stdout);
-            }
-            bytes = recv(s, &push_msg, sizeof(push_msg), MSG_DONTWAIT);
+        // Configura os descritores para select()
+        FD_ZERO(&readfds);
+        FD_SET(s, &readfds);              // monitora o socket
+        FD_SET(STDIN_FILENO, &readfds);   // monitora o stdin (teclado)
+        
+        max_fd = (s > STDIN_FILENO) ? s : STDIN_FILENO;
+        
+        // Espera atividade em qualquer um dos descritores
+        if (select(max_fd + 1, &readfds, NULL, NULL, NULL) < 0) {
+            perror("select");
+            break;
         }
         
-        printf("> ");
-        fflush(stdout);
-
-        if (fgets(input, BUFSZ, stdin) == NULL)
-            break;
-
-        input[strcspn(input, "\n")] = '\0';
-        if (strlen(input) == 0)
-            continue;
-
-        if (sscanf(input, "%s %[^\n]", command, argument) < 1)
-            continue;
-
-        memset(&msg, 0, sizeof(msg));
-
-        if (strcmp(command, "exit") == 0)
-        {
-            msg.type = MSG_END;
-            send(s, &msg, sizeof(msg), 0);
-            break;
+        // Verifica se há dados no socket (notificação do servidor)
+        if (FD_ISSET(s, &readfds)) {
+            Message push_msg;
+            bytes = recv(s, &push_msg, sizeof(push_msg), MSG_DONTWAIT);
+            while (bytes > 0) {
+                if (push_msg.type == MSG_PUSH) {
+                    printf("\n[NOTIFICATION] @%s: \"%s\"\n", 
+                           push_msg.username, push_msg.content);
+                    fflush(stdout);
+                    printf("> ");
+                    fflush(stdout);
+                }
+                bytes = recv(s, &push_msg, sizeof(push_msg), MSG_DONTWAIT);
+            }
         }
-        else if (strcmp(command, "POST") == 0)
-        {
-            msg.type = MSG_POST;
-            strncpy(msg.username, argv[3], USER_SIZE);
-            strncpy(msg.content, argument, CONTENT_SIZE);
-            send(s, &msg, sizeof(msg), 0);
-        }
-        else if (strcmp(command, "FOLLOW") == 0)
-        {
-            msg.type = MSG_FOLLOW;
-            strncpy(msg.username, argv[3], USER_SIZE);
-            strncpy(msg.content, argument, CONTENT_SIZE);
-            send(s, &msg, sizeof(msg), 0);
-        }
-        else if (strcmp(command, "READ") == 0)
-        {
-            msg.type = MSG_READ;
-            send(s, &msg, sizeof(msg), 0);
-            // REMOVI o loop while aqui - as mensagens serão recebidas
-            // no início do próximo loop do while(1)
-        }
-        else if (strcmp(command, "help") == 0)
-        {
-            printf("\nComandos:\n");
-            printf("  POST <texto> - Publica uma mensagem\n");
-            printf("  FOLLOW @user - Segue um usuário\n");
-            printf("  READ - Lê o feed histórico\n");
-            printf("  exit - Encerra conexão\n\n");
-        }
-        else
-        {
-            printf("Comando desconhecido. Use 'help'\n");
+        
+        // Verifica se há comando do usuário
+        if (FD_ISSET(STDIN_FILENO, &readfds)) {
+            printf("> ");
+            fflush(stdout);
+            
+            if (fgets(input, BUFSZ, stdin) == NULL)
+                break;
+
+            input[strcspn(input, "\n")] = '\0';
+            if (strlen(input) == 0)
+                continue;
+
+            if (sscanf(input, "%s %[^\n]", command, argument) < 1)
+                continue;
+
+            memset(&msg, 0, sizeof(msg));
+
+            if (strcmp(command, "exit") == 0)
+            {
+                msg.type = MSG_END;
+                send(s, &msg, sizeof(msg), 0);
+                break;
+            }
+            else if (strcmp(command, "POST") == 0)
+            {
+                msg.type = MSG_POST;
+                strncpy(msg.username, argv[3], USER_SIZE);
+                strncpy(msg.content, argument, CONTENT_SIZE);
+                send(s, &msg, sizeof(msg), 0);
+            }
+            else if (strcmp(command, "FOLLOW") == 0)
+            {
+                msg.type = MSG_FOLLOW;
+                strncpy(msg.username, argv[3], USER_SIZE);
+                strncpy(msg.content, argument, CONTENT_SIZE);
+                send(s, &msg, sizeof(msg), 0);
+            }
+            else if (strcmp(command, "READ") == 0)
+            {
+                msg.type = MSG_READ;
+                send(s, &msg, sizeof(msg), 0);
+            }
+            else if (strcmp(command, "help") == 0)
+            {
+                printf("\nComandos:\n");
+                printf("  POST <texto> - Publica uma mensagem\n");
+                printf("  FOLLOW @user - Segue um usuário\n");
+                printf("  READ - Lê o feed histórico\n");
+                printf("  exit - Encerra conexão\n\n");
+            }
+            else
+            {
+                printf("Comando desconhecido. Use 'help'\n");
+            }
         }
     }
 
     close(s);
     printf("Cliente encerrado.\n");
-    return 0;}
+    return 0;
+}
