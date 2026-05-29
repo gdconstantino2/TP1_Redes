6a5c7a62f4b4e32ccf0a0d36f34c34c116565923
gdconstantino2 <125415504+gdconstantino2@users.noreply.github.com>
2026-05-29 20:22:13 -0300

    Refactor conditional statements for clarity

--- arianagram/client-mt.c
+++ arianagram/client-mt.c
@@ -18,40 +18,34 @@ void usage(int argc, char **argv)
 
 int main(int argc, char **argv)
 {
-    if (argc != 4)
-    {
+    if (argc != 4) {
         usage(argc, argv);
     }
 
     struct sockaddr_storage storage;
-    if (addrparse(argv[1], argv[2], &storage) != 0)
-    {
+    if (addrparse(argv[1], argv[2], &storage) != 0) {
         usage(argc, argv);
     }
 
     int s = socket(storage.ss_family, SOCK_STREAM, 0);
     ssize_t bytes;
-    if (s == -1)
-    {
+    if (s == -1) {
         logexit("socket");
     }
 
     struct sockaddr *addr = (struct sockaddr *)(&storage);
-    if (connect(s, addr, sizeof(storage)) != 0)
-    {
+    if (connect(s, addr, sizeof(storage)) != 0) {
         logexit("connect");
     }
 
     printf("Conectado ao servidor Arianagram!\n");
-    
 
     Message msg;
     memset(&msg, 0, sizeof(msg));
     msg.type = MSG_CONNECT;
     strncpy(msg.username, argv[3], USER_SIZE);
 
-    if (send(s, &msg, sizeof(msg), 0) != sizeof(msg))
-    {
+    if (send(s, &msg, sizeof(msg), 0) != sizeof(msg)) {
         logexit("send connect");
     }
 
@@ -63,39 +57,35 @@ int main(int argc, char **argv)
     printf("  READ - Lê o feed histórico\n");
     printf("  exit - Encerra conexão\n");
     printf("  help - Mostra ajuda\n\n");
-    printf("> ");
-    fflush(stdout);
 
     char input[BUFSZ];
     char command[BUFSZ];
     char argument[BUFSZ];
-    
-    // Configura socket como não-bloqueante
+
     int flags = fcntl(s, F_GETFL, 0);
     fcntl(s, F_SETFL, flags | O_NONBLOCK);
-    
+
     fd_set readfds;
     int max_fd;
 
-    while (1)
-    {
+    while (1) {
         FD_ZERO(&readfds);
         FD_SET(s, &readfds);
         FD_SET(STDIN_FILENO, &readfds);
-        
+
         max_fd = (s > STDIN_FILENO) ? s : STDIN_FILENO;
-        
+
         if (select(max_fd + 1, &readfds, NULL, NULL, NULL) < 0) {
             perror("select");
             break;
         }
-        
+
         if (FD_ISSET(s, &readfds)) {
             Message push_msg;
             bytes = recv(s, &push_msg, sizeof(push_msg), MSG_DONTWAIT);
             while (bytes > 0) {
                 if (push_msg.type == MSG_PUSH) {
-                    printf("\n[NOTIFICATION] %s: \"%s\"\n", 
+                    printf("\n[NOTIFICATION] @%s: \"%s\"\n",
                            push_msg.username, push_msg.content);
                     fflush(stdout);
                     printf("> ");
@@ -104,50 +94,46 @@ int main(int argc, char **argv)
                 bytes = recv(s, &push_msg, sizeof(push_msg), MSG_DONTWAIT);
             }
         }
-        
+
         if (FD_ISSET(STDIN_FILENO, &readfds)) {
             printf("> ");
             fflush(stdout);
-            
-            if (fgets(input, BUFSZ, stdin) == NULL)
+
+            if (fgets(input, BUFSZ, stdin) == NULL) {
                 break;
+            }
 
             input[strcspn(input, "\n")] = '\0';
-            if (strlen(input) == 0)
+            if (strlen(input) == 0) {
                 continue;
+            }
 
-            if (sscanf(input, "%s %[^\n]", command, argument) < 1)
+            if (sscanf(input, "%s %[^\n]", command, argument) < 1) {
                 continue;
+            }
 
             memset(&msg, 0, sizeof(msg));
 
-            if (strcmp(command, "exit") == 0)
-            {
+            if (strcmp(command, "exit") == 0) {
                 msg.type = MSG_END;
                 send(s, &msg, sizeof(msg), 0);
                 break;
-            }
-            else if (strcmp(command, "POST") == 0)
-            {
+            } else if (strcmp(command, "POST") == 0) {
                 msg.type = MSG_POST;
                 strncpy(msg.username, argv[3], USER_SIZE);
                 strncpy(msg.content, argument, CONTENT_SIZE);
                 send(s, &msg, sizeof(msg), 0);
-            }
-            else if (strcmp(command, "FOLLOW") == 0)
-            {
+            } else if (strcmp(command, "FOLLOW") == 0) {
                 msg.type = MSG_FOLLOW;
                 strncpy(msg.username, argv[3], USER_SIZE);
                 strncpy(msg.content, argument, CONTENT_SIZE);
                 send(s, &msg, sizeof(msg), 0);
-            }
-            else if (strcmp(command, "READ") == 0)
-            {
+            } else if (strcmp(command, "READ") == 0) {
                 msg.type = MSG_READ;
                 send(s, &msg, sizeof(msg), 0);
-                
+
                 usleep(100000);
-                
+
                 int count = 0;
                 Message feed_msg;
                 bytes = recv(s, &feed_msg, sizeof(feed_msg), MSG_DONTWAIT);
@@ -159,27 +145,19 @@ int main(int argc, char **argv)
                     }
                     bytes = recv(s, &feed_msg, sizeof(feed_msg), MSG_DONTWAIT);
                 }
-                
+
                 if (count == 0) {
                     printf("[FEED] Nenhuma mensagem no feed.\n");
                 }
                 fflush(stdout);
-            }
-            else if (strcmp(command, "help") == 0)
-            {
+            } else if (strcmp(command, "help") == 0) {
                 printf("\nComandos:\n");
                 printf("  POST <texto> - Publica uma mensagem\n");
                 printf("  FOLLOW @user - Segue um usuário\n");
                 printf("  READ - Lê o feed histórico\n");
                 printf("  exit - Encerra conexão\n\n");
-                printf("> ");
-                fflush(stdout);
-            }
-            else
-            {
+            } else {
                 printf("Comando desconhecido. Use 'help'\n");
-                printf("> ");
-                fflush(stdout);
             }
         }
     }
