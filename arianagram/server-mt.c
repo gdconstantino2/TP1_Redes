93ec6ed9aef914feecaa3c429f7a82f371b190e1
gdconstantino2 <125415504+gdconstantino2@users.noreply.github.com>
2026-05-29 22:47:37 -0300

    Fix log message formatting in server-mt.c

--- arianagram/server-mt.c
+++ arianagram/server-mt.c
@@ -154,7 +154,7 @@ void *client_thread(void *data)
                     feed_count++;
                 }
 
-                printf("[LOG] @%s posted (ID %u): \"%s\"\n", msg.username, id, msg.content);
+                printf("[LOG] %s posted (ID %u): \"%s\"\n", msg.username, id, msg.content);
 
                 pthread_mutex_lock(&follows_mutex);
                 FollowNode *f = follows;
